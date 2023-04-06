ddd381935eee31a8bd51117503605a71d0d00c17
mixa <mihsa11ru@gmail.com>
2023-04-07 00:20:06 +0300

    omp

--- main.py
+++ main.py
@@ -4,21 +4,23 @@ from timeit import default_timer as timer
 import time
 import os
 import sys
-inf=int(sys.argv[1])
-num=int(sys.argv[2])#количество запускаемых алгоритмов
-names=["hash3","rabin_karp","rabin_karp_vect","hash3_seq","rabin_karp_seq","rabin_karp_seq_vect"]#алгоритмы
+num= list(map(int, sys.argv[1:]))
+inf=num[0]
+num=num[1:]
+names=["hash3","rabin_karp","rabin_karp_vect","hash3_seq","rabin_karp_seq","rabin_karp_seq_vect"]
 os.environ["OMP_PLACES"]="threads"
 os.environ["OMP_PROC_BIND"]="spread"
+os.environ["OMP_NUM_THREADS"]="32"
 i=10;
 out=open("time_result.txt",'w');
 out.write("size ")
-for j in range (num): out.write(names[j]+" ")
+for j in num: out.write(names[j]+" ")
 out.write("\n")
 while(i<inf):
     out.write(str(i)+" ");
     file="genome_samples/s"+str(i)+".txt"
-    for j in range (num):
-         subprocess.run(["build/Release/"+names[j],file,"1"])
+    for j in num:
+         subprocess.call(["build/"+names[j],file,"1"])
          tm=open("tmp.txt").read()
          out.write(tm[:-1]+" ")
     i+=i//10
