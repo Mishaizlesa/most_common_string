ef077d7445b67536d30fed0460d20f6c6645ecf6
Mishaizlesa <misha11ru@gmail.com>
2024-03-23 00:48:00 +0300

    1st

--- lib/src/naive.cpp
+++ lib/src/naive.cpp
@@ -10,27 +10,27 @@ extern void naive(std::vector<uint32_t>& freq ,const std::string& input_file, co
     fin>>data_;
     ll size=data_.size();
     int len=len_;
-    size_t VECTOR_LENGHT = 32;
+    size_t VECTOR_LENGHT = 64;
     uint8_t cycles = len/VECTOR_LENGHT;
     uint8_t leftover = len%VECTOR_LENGHT;
 
 
     std::unordered_map<int8_t,int8_t>symbols_code{{'A',int8_t(0)}, {'C',int8_t(1)},{'G',int8_t(2)},{'T',int8_t(3)}};
     freq.resize(size);
-    int8_t data[size];
+    uint8_t data[size];
     for(int i=0;i<size;++i){
         data[i]=symbols_code[data_[i]];
     }
 
 
     double start = omp_get_wtime();
-    #pragma omp parallel for shared(data, freq) proc_bind(close)
+    #pragma omp parallel for shared(data, freq) proc_bind(spread)
         for (int i=0;i<size-len+1;++i){
             uint32_t res=0;
             for(int j=0;j<size-len+1;++j){
                 int is_eq=1;
-                int8_t* pattern_1 = &data[i];
-                int8_t* pattern_2 = &data[j];
+                uint8_t* pattern_1 = &data[i];
+                uint8_t* pattern_2 = &data[j];
             #ifndef COMPARASION_VECTORIZED
                 for(int k=0;k<len;++k){
                     if (pattern_1[k]!=pattern_2[k]){
@@ -40,11 +40,11 @@ extern void naive(std::vector<uint32_t>& freq ,const std::string& input_file, co
             #else
                 for(int k=0;k<cycles;++k){
                     
-                    __m256i vpattern_1 = _mm256_load_si256((__m256i *)pattern_1);                    
-                    __m256i vpattern_2 = _mm256_load_si256((__m256i *)pattern_2);
-                    __m256i pcmp = _mm256_cmpeq_epi8(vpattern_1, vpattern_2); 
-                    uint8_t bitmask = _mm256_movemask_epi8(pcmp);
-                    if (bitmask != 0xffffffffU) {
+                    __m512i vpattern_1 = _mm512_loadu_epi8 ((void*) pattern_1);                
+                    __m512i vpattern_2 = _mm512_loadu_epi8 ((void*) pattern_2);
+                    __mmask64 pcmp _mm512_cmpeq_epu8_mask (vpattern_1, vpattern_2);
+                    unsigned __int64 bitmask =  _cvtmask64_u64 (pcmp);
+                    if (bitmask != 0xffffffffffffffffU) {
                         is_eq=0;
                         break;
                     }
@@ -75,5 +75,4 @@ extern void naive(std::vector<uint32_t>& freq ,const std::string& input_file, co
     }
     return;
 }
-
 
